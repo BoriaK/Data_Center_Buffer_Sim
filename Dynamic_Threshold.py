458951b4ea60adc9e4a5681f4e3adf9fba43bd63
Boris <boria21@gmail.com>
2022-07-22 06:56:28 +0300

    added torch.minimum to Q

--- Dynamic_Threshold.py
+++ Dynamic_Threshold.py
@@ -16,53 +16,61 @@ from DC_Traffic_Generator.Chaotic_Map_Generator import genDataset
 
 # for multiple streams
 
-N_streams = 3  # number of streams on each port
 N_ports = 2  # number of ports on the switch
+max_Queues = 3  # the maximum number of queues per port
+# N_streams = torch.randint(max_Queues, (N_ports,))  # number of streams on each port
+N_streams = [1, 3]
 Length = 1000  # sequence length
 alpha_high = 2  # alpha for high priority queues
 alpha_low = 1  # alpha for low priority queues
 B = 60  # full buffer capacity (for a single switch), 60 packets
-Traffic = torch.zeros(N_ports, N_streams, Length)
+Traffic = torch.zeros(N_ports, max_Queues, Length)
 # print(Q.shape)
-Traffic[0, 0, :] = genDataset(d=0.2, seq_len=1000)  # generate 1/3 stream on port1
-Traffic[1, 0, :] = genDataset(d=0.2, seq_len=1000)  # generate 3 different streams on port2
-Traffic[1, 1, :] = genDataset(d=0.2, seq_len=1000)
-Traffic[1, 2, :] = genDataset(d=0.2, seq_len=1000)
+for i in range(N_ports):
+    for j in range(N_streams[i]):
+        Traffic[i, j, :] = genDataset(d=0.2, seq_len=1000)  # generate 1/3 stream on port1
+        # Traffic[1, 0, :] = genDataset(d=0.2, seq_len=1000)  # generate 3 different streams on port2
+        # Traffic[1, 1, :] = genDataset(d=0.2, seq_len=1000)
+        # Traffic[1, 2, :] = genDataset(d=0.2, seq_len=1000)
 
-Q = Traffic.sum((0, 1))
+Q = torch.min(Traffic.sum((0, 1)), torch.ones(Length) * 60)
 
-Threshold_1h = alpha_high * (B - Q)
-Threshold_2h = alpha_high * (B - Q)
-Threshold_2l = alpha_low * (B - Q)
-
+Threshold_h = alpha_high * (B - Q)
+Threshold_l = alpha_low * (B - Q)
+# Threshold_1h = alpha_high * (B - Q)
+# Threshold_2h = alpha_high * (B - Q)
+# Threshold_2l = alpha_low * (B - Q)
 
 Time = range(0, len(Traffic[0, 0, :]))
 plt.figure()
-plt.subplot(4, 1, 1)
-plt.plot(Time, Traffic[0, 0, :], Time, Threshold_1h)
-plt.xlabel('Time [samples]')
-plt.ylabel('Packets')
-plt.grid()
-plt.legend(['q_1_h', 'T_1_h'])
-plt.subplot(4, 1, 2)
-plt.plot(Time, Traffic[1, 0, :], Time, Threshold_2h)
-plt.xlabel('Time [samples]')
-plt.ylabel('Packets')
-plt.grid()
-plt.legend(['q_2_h', 'T_2_h'])
-plt.subplot(4, 1, 3)
-plt.plot(Time, Traffic[1, 1, :], Time, Threshold_2l)
-plt.xlabel('Time [samples]')
-plt.ylabel('Packets')
-plt.grid()
-plt.legend(['q_2_l_1', 'T_2_l'])
-plt.subplot(4, 1, 4)
-plt.plot(Time, Traffic[1, 2, :], Time, Threshold_2l)
-plt.xlabel('Time [samples]')
-plt.ylabel('Packets')
-plt.grid()
-plt.legend(['q_2_l_2', 'T_2_l'])
-plt.show()
+for k in range(N_ports * max_Queues):
+    plt.subplot(N_ports * max_Queues, 1, k)
+    if k == 0 or k == 3:
+        plt.plot(Time, Traffic[0, 0, :], Time, Threshold_h)
+        plt.legend(['q_1_h', 'T_1_h'])
+    plt.xlabel('Time [samples]')
+    plt.ylabel('Packets')
+    plt.grid()
+
+    # plt.subplot(4, 1, 2)
+    # plt.plot(Time, Traffic[1, 0, :], Time, Threshold_2h)
+    # plt.xlabel('Time [samples]')
+    # plt.ylabel('Packets')
+    # plt.grid()
+    # plt.legend(['q_2_h', 'T_2_h'])
+    # plt.subplot(4, 1, 3)
+    # plt.plot(Time, Traffic[1, 1, :], Time, Threshold_2l)
+    # plt.xlabel('Time [samples]')
+    # plt.ylabel('Packets')
+    # plt.grid()
+    # plt.legend(['q_2_l_1', 'T_2_l'])
+    # plt.subplot(4, 1, 4)
+    # plt.plot(Time, Traffic[1, 2, :], Time, Threshold_2l)
+    # plt.xlabel('Time [samples]')
+    # plt.ylabel('Packets')
+    # plt.grid()
+    # plt.legend(['q_2_l_2', 'T_2_l'])
+    plt.show()
 
 # print(Threshold.shape)
 
